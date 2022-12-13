65bff563104a5536e7450c7f7c3b50587855d3dd
Роман Гольцман <romnasi@gmail.com>
2022-12-14 02:49:00 +0300

    feat: update function showCommandError

--- src/message/message.js
+++ src/message/message.js
@@ -1,4 +1,11 @@
-export const showCommandError = () => process.stdout.write(`Operation failed\n`);
+export const showCommandError = (clarifyingMessage = '') => {
+  if (!clarifyingMessage) {
+    process.stdout.write(`Operation failed\n`)
+  } else {
+    const message = `Operation failed: ${clarifyingMessage}\n`;
+    process.stdout.write(message);
+  }
+};
 
 export const showCurrentDir = (dir) => {
   process.stdout.write(`\nYou are currently in ${dir}\n`);
